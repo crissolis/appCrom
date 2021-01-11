67a3cc2a1315e694aea4802cb74dfe24d7442707
chsolis <csolis@development-solutions.com>
2021-01-10 21:22:57 -0500

    make it better

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    host:'localhost',
-    user:'postgres',
-    password:'Desarrollo2019',
-    database:'noticiasApp',
-    port:'5432'
+    // host:'localhost',
+    // user:'postgres',
+    // password:'Desarrollo2019',
+    // database:'noticiasApp',
+    // port:'5432'
 });
 
 module.exports = {
